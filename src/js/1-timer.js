73cf2b54d09cb8905e48be82817a07bd50d8975e
pilipchu <114575426+pilipchu@users.noreply.github.com>
2024-03-17 21:59:34 +0200

    add

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -26,7 +26,7 @@ const options = {
                 message: 'Please choose a date in the future',
                 maxWidth: 400,
                 position: "topRight",
-                iconUrl: '../img/close_black_24dp.svg'
+                iconUrl: './img/close_black_24dp.svg'
             });
             clearInterval(intervalId)
             textDays.textContent = '00'
